c78a00e3c2d0eba1aa1153b9e4c01b9e2e5d3c5c
blahii <blahiidima@gmail.com>
2024-04-03 15:05:40 +0200

    update

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -18,6 +18,28 @@ export class ProjectsController {
     @Query('furnishing') furnishing: string = '',
     @Query('min') minrangePrice: string = '',
     @Query('max') maxrangePrice: string = '',
+    @Query('year') year: string = '',
+    @Query('quarter') quarter: string = '',
+    @Query('month') month: string = '',
+    @Query('twoBedroom') twoBedroom: string,
+    @Query('sixBedroom') sixBedroom: string,
+    @Query('oneBedroomPool') oneBedroomPool: string,
+    @Query('oneBedroom') oneBedroom: string,
+    @Query('fourBedroom') fourBedroom: string,
+    @Query('threeBedroom') threeBedroom: string,
+    @Query('mergedStudios') mergedStudios: string,
+    @Query('twoBedroomPool') twoBedroomPool: string,
+    @Query('eightBedroom') eightBedroom: string,
+    @Query('nineBedroom') nineBedroom: string,
+    @Query('sevenBedroom') sevenBedroom: string,
+    @Query('studioPool') studioPool: string,
+    @Query('fiveBedroom') fiveBedroom: string,
+    @Query('threeBedroomPool') threeBedroomPool: string,
+    @Query('twoAndHalfBedroom') twoAndHalfBedroom: string,
+    @Query('studio') studio: string,
+    @Query('oneAndHalfBedroom') oneAndHalfBedroom: string,
+    @Query('threeAndHalfBedroom') threeAndHalfBedroom: string,
+
   ) {
     const skip = page ? (parseInt(page) - 1) * parseInt(perPage || '24') : 0;
     const take = perPage ? parseInt(perPage) : 24;
@@ -27,6 +49,9 @@ export class ProjectsController {
     const regionsValues = regions ? regions.split(',') : [];
     const areaValues = units_types ? units_types.split(',') : [];
     const whereClause = {
+      Year: year || undefined,  // Задаємо undefined, якщо параметр пустий
+      Quarter: quarter || undefined,
+      Month: month || undefined,
       Project_name: { contains: search },
       Developers_name: developerName.length ? { in: developerName } : undefined,
       Region: regionsValues.length ? { in: regionsValues } : undefined,
@@ -37,7 +62,31 @@ export class ProjectsController {
       Furnishing: { contains: furnishing },
       Price_from_AED: { contains: minrangePrice },
       Price_to_AED: { contains: maxrangePrice },
+      ...(twoBedroom === 'true' && { TwoBedroom: { not: 'false' } }),
+      ...(sixBedroom === 'true' && { SixBedroom: { not: 'false' } }),
+      ...(oneBedroomPool === 'true' && { OneBedroomPool: { not: '' } }),
+      ...(oneBedroom === 'true' && { OneBedroom: { not: 'false' } }),
+      ...(fourBedroom === 'true' && { FourBedroom: { not: 'false' } }),
+      ...(threeBedroom === 'true' && { ThreeBedroom: { not: 'false' } }),
+      ...(mergedStudios === 'true' && { MergedStudios: { not: 'false' } }),
+      ...(twoBedroomPool === 'true' && { TwoBedroomPool: { not: 'false' } }),
+      ...(eightBedroom === 'true' && { EightBedroom: { not: 'false' } }),
+      ...(nineBedroom === 'true' && { NineBedroom: { not: 'false' } }),
+      ...(sevenBedroom === 'true' && { SevenBedroom: { not: 'false' } }),
+      ...(studioPool === 'true' && { StudioPool: { not: 'false' } }),
+      ...(fiveBedroom === 'true' && { FiveBedroom: { not: 'false' } }),
+      ...(threeBedroomPool === 'true' && { ThreeBedroomPool: { not: 'false' } }),
+      ...(twoAndHalfBedroom === 'true' && { TwoAndHalfBedroom: { not: 'false' } }),
+      ...(studio === 'true' && { Studio: { not: 'false' } }),
+      ...(oneAndHalfBedroom === 'true' && { OneAndHalfBedroom: { not: 'false' } }),
+      ...(threeAndHalfBedroom === 'true' && { ThreeAndHalfBedroom: { not: 'false' } }),
+
     };
+
+    if (year) whereClause.Year = year;
+    if (quarter) whereClause.Quarter = quarter;
+    if (month) whereClause.Month = month;
+
     const projects = await this.prisma.projects.findMany({
       where: whereClause,
       skip,
