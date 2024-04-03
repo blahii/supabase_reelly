f73ca1aea616f77b0eb4baa752d304ce0a11a73b
blahii <blahiidima@gmail.com>
2024-04-03 17:31:54 +0200

    update

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -12,35 +12,12 @@ export class ProjectsController {
     @Query('regions') regions: string = '',
     @Query('units_types') units_types: string = '',
     @Query('area_name') name: string = '',
-    @Query('display_all') dispall: string = '',
     @Query('status') status: string = '',
     @Query('developer') developer: string = '',
     @Query('priority') priority: string = '',
     @Query('furnishing') furnishing: string = '',
     @Query('min') minrangePrice: string = '',
     @Query('max') maxrangePrice: string = '',
-    @Query('year') year: string = '',
-    @Query('quarter') quarter: string = '',
-    @Query('month') month: string = '',
-    @Query('twoBedroom') twoBedroom: string,
-    @Query('sixBedroom') sixBedroom: string,
-    @Query('oneBedroomPool') oneBedroomPool: string,
-    @Query('oneBedroom') oneBedroom: string,
-    @Query('fourBedroom') fourBedroom: string,
-    @Query('threeBedroom') threeBedroom: string,
-    @Query('mergedStudios') mergedStudios: string,
-    @Query('twoBedroomPool') twoBedroomPool: string,
-    @Query('eightBedroom') eightBedroom: string,
-    @Query('nineBedroom') nineBedroom: string,
-    @Query('sevenBedroom') sevenBedroom: string,
-    @Query('studioPool') studioPool: string,
-    @Query('fiveBedroom') fiveBedroom: string,
-    @Query('threeBedroomPool') threeBedroomPool: string,
-    @Query('twoAndHalfBedroom') twoAndHalfBedroom: string,
-    @Query('studio') studio: string,
-    @Query('oneAndHalfBedroom') oneAndHalfBedroom: string,
-    @Query('threeAndHalfBedroom') threeAndHalfBedroom: string,
-
   ) {
     const skip = page ? (parseInt(page) - 1) * parseInt(perPage || '24') : 0;
     const take = perPage ? parseInt(perPage) : 24;
@@ -50,7 +27,6 @@ export class ProjectsController {
     const regionsValues = regions ? regions.split(',') : [];
     const areaValues = units_types ? units_types.split(',') : [];
     const whereClause = {
-     
       Project_name: { contains: search },
       Developers_name: developerName.length ? { in: developerName } : undefined,
       Region: regionsValues.length ? { in: regionsValues } : undefined,
@@ -61,31 +37,7 @@ export class ProjectsController {
       Furnishing: { contains: furnishing },
       Price_from_AED: { contains: minrangePrice },
       Price_to_AED: { contains: maxrangePrice },
-      Year: year ,  // Задаємо undefined, якщо параметр пустий
-      Quarter: quarter,
-      Month: month,
-      Display_all: dispall,
-      ...(twoBedroom === 'true' && { TwoBedroom: { not: 'false' } }),
-      ...(sixBedroom === 'true' && { SixBedroom: { not: 'false' } }),
-      ...(oneBedroomPool === 'true' && { OneBedroomPool: { not: '' } }),
-      ...(oneBedroom === 'true' && { OneBedroom: { not: 'false' } }),
-      ...(fourBedroom === 'true' && { FourBedroom: { not: 'false' } }),
-      ...(threeBedroom === 'true' && { ThreeBedroom: { not: 'false' } }),
-      ...(mergedStudios === 'true' && { MergedStudios: { not: 'false' } }),
-      ...(twoBedroomPool === 'true' && { TwoBedroomPool: { not: 'false' } }),
-      ...(eightBedroom === 'true' && { EightBedroom: { not: 'false' } }),
-      ...(nineBedroom === 'true' && { NineBedroom: { not: 'false' } }),
-      ...(sevenBedroom === 'true' && { SevenBedroom: { not: 'false' } }),
-      ...(studioPool === 'true' && { StudioPool: { not: 'false' } }),
-      ...(fiveBedroom === 'true' && { FiveBedroom: { not: 'false' } }),
-      ...(threeBedroomPool === 'true' && { ThreeBedroomPool: { not: 'false' } }),
-      ...(twoAndHalfBedroom === 'true' && { TwoAndHalfBedroom: { not: 'false' } }),
-      ...(studio === 'true' && { Studio: { not: 'false' } }),
-      ...(oneAndHalfBedroom === 'true' && { OneAndHalfBedroom: { not: 'false' } }),
-      ...(threeAndHalfBedroom === 'true' && { ThreeAndHalfBedroom: { not: 'false' } }),
-
     };
-
     const projects = await this.prisma.projects.findMany({
       where: whereClause,
       skip,
